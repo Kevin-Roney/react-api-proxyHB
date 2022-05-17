3082dddb8e96f812f2fa469616ffd1aaebd510d3
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-17 16:57:06 -0700

    filled out yelp.js, going to finish button to test it

--- functions/yelp.js
+++ functions/yelp.js
@@ -12,16 +12,23 @@ exports.handler = async (event, context) => {
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
-
+    const response = await fetch(`https://api.yelp.com/v3/businesses/search?location=portland`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.YELP_KEY}`,
+      },
+    });
     // consult the yelp docs to figure out how to use a city, state, and country to make a request for businesses
     // https://www.yelp.com/developers/documentation/v3/business_search
     // don't forget to add the yelp API key!
+    const data = await response.json();
+    console.log(data);
     
+
     return { 
       statusCode: 200, 
       headers,
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the yelp data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      body: JSON.stringify({ data }),
     };
   } catch (error) {
     console.log(error);
