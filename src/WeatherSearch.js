d0bf29fdd5665c229647a7a745495915c6498e39
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-18 10:58:25 -0700

    changed test params

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -8,7 +8,7 @@ export default function WeatherSearch() {
   const [weatherData, setWeatherData] = useState([]);
   const [weatherQuery, setWeatherQuery] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  console.log(weatherData);
+
   async function handleWeatherSubmit(e) {
     e.preventDefault();
       
