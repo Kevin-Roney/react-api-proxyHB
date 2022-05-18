e3928d05c4035d761d69a3b5ecefb1504b121a28
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-18 10:38:19 -0700

    added netlify functionality to weather.js

--- functions/weather.js
+++ functions/weather.js
@@ -12,20 +12,22 @@ exports.handler = async (event, context) => {
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
-
+    const geocodingResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${event.queryStringParameters.searchFilter}&appid=${process.env.WEATHER_KEY}`);
     // tragicly, we cannot just pass the city name to this API. it wants a latitude and longitude for the weather
     // consult the yelp docs to figure out how to use a city, state, and country to make a request and get the latitude and longitude
     // https://openweathermap.org/api/geocoding-api
-
+    const [{ lat, lon }] = await geocodingResponse.json();
     // once you have gotten the lat/lon using the geocoding api, use the lat/lon to get the weather. Consult the docs below:
     // https://openweathermap.org/api/one-call-api
+    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_KEY}`);
 
+    const weatherData = await weatherResponse.json();
 
     return { 
       statusCode: 200, 
       headers,
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      body: JSON.stringify({ weatherData }),
     };
   } catch (error) {
     console.log(error);
